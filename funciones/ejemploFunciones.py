8dc8727f151a7c930dd1e9eefd17392ddeeff445
dardonarioda <dar.donaire@duocuc.cl>
2026-05-30 11:33:41 -0400

    funciones en otro archivo

--- funciones/ejemploFunciones.py
+++ funciones/ejemploFunciones.py
@@ -1,29 +1,13 @@
-#Funciones
-
-def sumarDosNumeros():
-    #Esta funcion permite sumar dos numeros, ingresados dentro de la funcion
-
-    num1 = int(input("Ingrese número 1: "))
-    num2 = int(input("Ingrese número 2: "))
-
-    suma = num1 + num2
-
-    print(f"La suma de {num1} + {num2} es  = {suma}")
-
-def sumar(a,b) :
-    #Esta funcion permite sumar dos números ingresados por parametros
-
-
-    suma = a + b
-    return suma
+import funcionesDetalle as fn
+# from funcionesDetalle import sumar  (Es lo mismo que arriba , se llama directamente)
 
 #linea principal
-sumarDosNumeros()
+fn.sumarDosNumeros()
 
-res = sumar(4,5)
+res = fn.sumar(4,5)
 print(res)
 
 num1 = int(input("Número 1: "))
 num2 = int(input("Número 2: "))
-res = sumar(num1, num2)
+res = fn.sumar(num1, num2)
 print("La suma es:",res)
---
+++ funciones/funcionesDetalle.py
@@ -0,0 +1,18 @@
+#Funciones
+
+def sumarDosNumeros():
+    #Esta funcion permite sumar dos numeros, ingresados dentro de la funcion
+
+    num1 = int(input("Ingrese número 1: "))
+    num2 = int(input("Ingrese número 2: "))
+
+    suma = num1 + num2
+
+    print(f"La suma de {num1} + {num2} es  = {suma}")
+
+def sumar(a,b) :
+    #Esta funcion permite sumar dos números ingresados por parametros
+
+
+    suma = a + b
+    return suma
